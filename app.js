65960e4ed5307645f139047ff64901e687e55175
theodore_stamatiadis <stamatiadisteo@gmail.com>
2022-02-16 16:58:42 +0200

    added weather background videos and did some layout changes

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const container = document.querySelector(".container");
+const geo_error_container = document.querySelector(".geo_error");
+const weather_video_el = document.querySelector("#weather_video");
+const intro_text = document.querySelector(".geo_error>div p");
 const location_el = document.querySelector("#location");
 const country_el = document.querySelector("#country");
 const temp_el = document.querySelector("#temp");
@@ -7,6 +11,21 @@ const feel_el = document.querySelector("#feel_temp");
 const humidity_el = document.querySelector("#humidity");
 const pressure_el = document.querySelector("#pressure");
 const wind_speed_el = document.querySelector("#wind_speed");
+
+const weather_videos = 
+[
+    "day_clear_sky",
+    "day_few_clouds",
+    "day_many_clouds",
+    "day_snow",
+    "day_rain",
+    "night_clear_sky",
+    "night_few_clouds",
+    "night_many_clouds",
+    "night_rain",
+    "night_snow"
+]
+
 
 let latitude = 0;
 let longitude = 0;
@@ -19,6 +38,8 @@ else {
 }
 
 function getPosition(position) {
+    geo_error_container.style.display="none";
+    container.style.display = "flex";
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
 
@@ -33,7 +54,16 @@ function getPosition(position) {
 }
 
 function showError(error){
-    console.error(error);
+    switch(error.code) {
+        case 1:
+            intro_text.textContent = "Sorry, browser needs to know your location for the weather app";
+            break;
+        case 2:
+            intro_text.textContent = "Sorry,could't take your location,please try again";
+            break;
+        case 3:
+            intro_text.textContent = "Sorry, time allowed to aquire location information was reached before information was obtained,please try again";
+    }
 }
 
 async function fetchData(lat,lon) {
@@ -43,6 +73,7 @@ async function fetchData(lat,lon) {
 } 
 
 function showResults(data) {
+    pickVideo(parseInt(data.weather[0].id));
     location_el.textContent = data.name;
     country_el.textContent = data.sys.country;
 
@@ -54,6 +85,54 @@ function showResults(data) {
     humidity_el.textContent = `${data.main.humidity}%`;
     pressure_el.textContent = `${data.main.pressure}mbar`;
     wind_speed_el.textContent = `${toKm(data.wind.speed)}km/h`
+}
+
+function pickVideo(id) {
+    const time = new Date().getHours();
+    let weather_pick = "";
+    
+    if(time>=6 && time<=18) {
+        if(id<=531) {
+            weather_pick = "day_rain";
+        }
+        else if(id<=622) {
+            weather_pick = "day_snow";
+        }
+        else if(id===800) {
+            weather_pick = "day_clear_sky";
+        }
+        else {
+            if(id===801 || id===802) {
+                weather_pick = "day_few_clouds";
+            }
+            else {
+                weather_pick = "day_many_clouds";
+            }
+        }
+    }
+    else {
+        if(id<=531) {
+            weather_pick = "night_rain";
+        }
+        else if(id<=622) {
+            weather_pick = "night_snow";
+        }
+        else if(id===800) {
+            weather_pick = "night_clear_sky";
+        }
+        else {
+            if(id===801 || id===802) {
+                weather_pick = "night_few_clouds";
+            }
+            else {
+                weather_pick = "night_many_clouds";
+            }
+        }
+    }
+
+    weather_video_el.src = `videos/${weather_videos.find(w=>w===weather_pick)}.mp4`;
+    weather_video_el.parentNode.load();
+    weather_video_el.parentNode.play();
 }
 
 function toCelsius(val) {
